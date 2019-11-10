be7dd7927a10e8599057683b94ec8f19d30e7947
ktsha <ktsha@121.130.14.69>
2019-11-11 00:58:02 +0900

    은행리스트 업데이트

--- kakeibo/Account/views.py
+++ kakeibo/Account/views.py
@@ -56,6 +56,26 @@ def accountdel(request):
 # 통장수정
 def accountupdate(request):
     
-    return redirect("Account/accountreg.html") 
+    req_account_id = request.POST['account_id']
+    dbObj = AccountList.objects.get(account_id=req_account_id)
+    
+    req_account_name = request.POST['inp_account_name']
+    req_bank = request.POST['inp_bank_name']
+    req_account_num = request.POST['inp_account_num']
+    req_account_kind = request.POST['sel_account_kind']
+    req_account_kind_other = request.POST['inp_account_kind_other']
+    req_account_purpose = request.POST['sel_account_purpose']
+    req_account_purpose_other = request.POST['inp_account_purpose_other']
+    
+    dbObj.update(
+            account_name = req_account_name, 
+            bank = req_bank,
+            account_num = None if req_account_num == "" else int(req_account_num),
+            account_kind = None if req_account_kind == "" else int(req_account_kind),
+            account_kind_other = None if req_account_kind_other == "" else req_account_kind_other,
+            account_purpose = None if req_account_purpose == "" else int(req_account_purpose),
+            account_purpose_other = None if req_account_purpose_other == ""  else req_account_purpose_other)
+    
+    return redirect("/") 
 
     
